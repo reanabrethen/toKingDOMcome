ff0f57c1c654e1f2520dac7aae44e3e19452fb02
Admin <reanajb@gmail.com>
2024-01-29 10:20:54 -0500

    review & notes

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ removeLi()
         
         function changeFont(id, size){
            const element = document.querySelector(`#${id}`)
-           element.style.fontSize = `${size}`
+           element.style.fontSize = `${size}`   //do not need a template literal, can use just 'size' ; if you are doing something that is expecting a string, you don't have to use it as it does it for you
         }
 
 
@@ -70,8 +70,12 @@ function addToArguments(element){
 // 2. Put this function into action by creating an image element in JavaScript and passing it as a parameter to your function.
 
 const image = document.createElement('img')
-
+//image.src = 'https......com'  --> gives image a source so that it will show up on DOM
 addToArguments(image)
+
+
+
+
 
 // 3. Create a function that takes an image element as a parameter and changes its height to 30 pixels.
 
@@ -80,22 +84,40 @@ function changeImage(id){
     adjustHeight.style.height = '30px'
 }
 
+//WAY TO DO THE NUMBER 3&4 DUE TO THE READ ME
+//function changeImage(element){
+    //set element style
+    //element.style.height = '30px'
+// }
+//querying the element
+//const adjustHeight = document.querySelector(`#image-1`)
+//feed the element to the function
+// changeImage(adjustHeight)
+
+
 // 4. Implement this function by querying an image element and passing it as a parameter to the function.
 
 changeImage('image-1')
 
+
+
+
+
 // 5. Develop a function that accepts an element as a parameter and assigns it the class `invisible`.
 
 function makeInvisible(element){
+    //add the class to make invisible
       element.classList.add('invisible')
-
 }
 
 // 6. Now, query an element on the page and pass it as a parameter to this function. You will observe the element disappearing. (You can check the CSS file to understand how the `invisible` class functions.)
 
+//grab the element
 const hidden = document.querySelector('h1')
-
+//call function with element
 makeInvisible(hidden)
+
+
 
 
 
@@ -106,16 +128,21 @@ makeInvisible(hidden)
 // 1. Write a function that accepts text as a parameter and returns a new `<li>` with the provided text as its `innerText`.
 
 function createLi(text){
+    //creating new list element
     const newLi = document.createElement('li')
-    newLi.innerText = `${text}`
+    //setting text of list element
+    newLi.innerText = `${text}`     //doesn't need template literal here
+        //return the element
     return newLi
 }
 
-
 // 2. Test the function by calling it. Store the returned DOM element in a variable and then append it to the page using your function from Part Two.
+//set a variable
 const newPoint = createLi("You've got this")
-
+//calling the function from above with new item
 addToArguments(newPoint)
+
+
 
 
 
@@ -123,15 +150,13 @@ addToArguments(newPoint)
 
 function createHeader(size, text){
     const newHeader = document.createElement(`h${size}`)
-    newHeader.innerText = `${text}`
+    newHeader.innerText = text
     return newHeader
 }
 
-
-
 // 4. Test the function by calling it with a header size and some text, storing the return value in a variable, and then appending it to the page using your function from Part Two.
 
-const newHead = createHeader(3, 'Holy S#%T! A NEW HEAD')
+const newHead = createHeader(2, 'Holy S#%T! A NEW HEAD')
 addToArguments(newHead)
 
 
